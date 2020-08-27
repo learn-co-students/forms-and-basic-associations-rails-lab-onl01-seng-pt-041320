01a0554cc24045f8d35ec2562b9d6dba2b017954
OrkunSA <c.orkun.s@gmail.com>
2020-08-26 20:19:38 -0500

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,12 @@
 class Genre < ActiveRecord::Base
-  # add associations
+  has_many :songs
+  has_many :artists, through: :songs 
+
+  def song_ids=(ids)
+    ids.each do |id|
+      song = Song.find(id)
+      self.songs << song
+    end
+  end
+  
 end
