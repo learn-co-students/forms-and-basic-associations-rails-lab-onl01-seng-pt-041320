ee2365dd49cb0a9570265d74de4123a7ac7bf28e
MakoaDeVincent <makoadevincent@gmail.com>
2020-09-04 14:46:23 -1000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,35 @@
+
 class Song < ActiveRecord::Base
   # add associations here
+  has_many :notes
+  belongs_to :artist
+  belongs_to :genre
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def note_shapes=(shapes)
+    shapes.each do |shape|
+      if shape.strip != ''
+        self.notes.build(content: shape)
+      end
+    end
+  end
+
+  def note_shapes
+    self.notes.map(&:content)
+  end
 end
